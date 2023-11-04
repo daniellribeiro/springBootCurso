6aacb8fa7ec01d64fe21d8bb5fbf5ca8ccd9dc6b
daniellribeiro <daniellopes03@hotmail.com>
2023-11-04 18:13:22 -0300

    Commit

---
+++ vendas/src/main/java/com/github/daniellribeiro/repository/ClienteRepositorio.java
@@ -0,0 +1,12 @@
+package com.github.daniellribeiro.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.github.daniellribeiro.model.Cliente;
+
+@Repository
+public class ClienteRepositorio {
+	public void persistir(Cliente cliente) {
+		
+	}
+}
