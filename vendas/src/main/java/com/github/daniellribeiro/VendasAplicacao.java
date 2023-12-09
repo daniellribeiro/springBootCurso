7114a33f1ea2e40e8c8320d919758dd213b5a1a4
daniellribeiro <daniellopes03@hotmail.com>
2023-12-08 21:30:52 -0300

    Commit

--- vendas/src/main/java/com/github/daniellribeiro/VendasAplicacao.java
+++ vendas/src/main/java/com/github/daniellribeiro/VendasAplicacao.java
@@ -1,5 +1,7 @@
 package com.github.daniellribeiro;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,9 +22,29 @@ public class VendasAplicacao {
 	@Bean
 	public CommandLineRunner init(@Autowired Clientes clientes) {
 		return args -> {
-			Cliente cliente = new Cliente();
-			cliente.setNome("Daniel");
-			clientes.salvar(cliente);
+			clientes.salvar(new Cliente("Daniel"));
+			clientes.salvar(new Cliente("Jose"));
+			clientes.salvar(new Cliente("Teste"));
+			clientes.salvar(new Cliente("Gabriel"));
+			clientes.salvar(new Cliente("Matheus"));
+			
+			clientes.atualizar("Teste",2);
+			
+			clientes.deletar(4);
+			
+			clientes.buscarClientePorId(1).forEach(System.out::println);
+
+			clientes.buscarClientePorNome("Matheu").forEach(System.out::println);
+			
+			System.out.println("TODOS:");
+			clientes.buscarTodos().forEach(System.out::println);
+			
+			clientes.deletar(1);
+			clientes.deletar(2);
+			clientes.deletar(3);
+			clientes.deletar(5);
+			
+			System.out.println(clientes.buscarTodos().isEmpty() ? "Vazio" : "Tem valor");
 		};
 	}
 	
--- vendas/src/main/java/com/github/daniellribeiro/domain/repository/Clientes.java
+++ vendas/src/main/java/com/github/daniellribeiro/domain/repository/Clientes.java
@@ -1,19 +1,62 @@
 package com.github.daniellribeiro.domain.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.github.daniellribeiro.domain.entity.Cliente;
 
 @Repository
 public class Clientes {
+	private static String SELECT = "SELECT * FROM CLIENTE";
+
 	private static String INSERT = "INSERT INTO CLIENTE(NOME) VALUES (?)";
-	
+
+	private static String UPDATE = "UPDATE CLIENTE SET NOME = (?) WHERE ID = (?)";
+
+	private static String DELETE = "DELETE FROM CLIENTE WHERE ID = (?)";
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	public void salvar(Cliente cliente) {
-		jdbcTemplate.update(INSERT,new Object[] {cliente.getNome()});
+
+	public void deletar(Integer id) {
+		jdbcTemplate.update(DELETE, new Object[] { id });
 	}
+
+	public void atualizar(String nome, Integer id) {
+		jdbcTemplate.update(UPDATE, new Object[] { nome, id });
+	}
+
+	public void salvar(Cliente cliente) {
+		jdbcTemplate.update(INSERT, new Object[] { cliente.getNome() });
+	}
+
+	public List<Cliente> buscarTodos() {
+		return jdbcTemplate.query(SELECT, obterClienteMapper());
+	}
+
+	public List<Cliente> buscarClientePorId(Integer id) {
+		return jdbcTemplate.query(SELECT.concat(" WHERE id = (?)"), new Object[] { id }, obterClienteMapper());
+
+	}
+
+	public List<Cliente> buscarClientePorNome(String nome) {
+		return jdbcTemplate.query(SELECT.concat(" WHERE nome like (?)"), new Object[] { "%" + nome + "%" },
+				obterClienteMapper());
+	}
+
+	public RowMapper<Cliente> obterClienteMapper() {
+		return new RowMapper<Cliente>() {
+			@Override
+			public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+				return new Cliente(resultSet.getInt("id"), resultSet.getString("nome"));
+			};
+		};
+	}
+
 }
