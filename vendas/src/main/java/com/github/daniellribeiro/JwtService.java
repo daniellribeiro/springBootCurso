3198496deccefb14bc98c6ec95754360f128989e
Daniel Ribeiro <daniellopes03@hotmail.com>
2024-04-20 08:57:36 -0300

    commit

--- vendas/src/main/java/com/github/daniellribeiro/JwtService.java
+++ vendas/src/main/java/com/github/daniellribeiro/JwtService.java
@@ -1,11 +1,16 @@
 package com.github.daniellribeiro;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.github.daniellribeiro.domain.entity.Usuario;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class JwtService {
@@ -13,12 +18,17 @@ public class JwtService {
 	private String expiracao;
 	@Value("$(security.jwt.chave-assinatura")
 	private String chaveAssinatura;
-	
+
 	private String gerarToken(Usuario usuario) {
 		long expString = Long.valueOf(expiracao);
 		LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+
+		Date data = Date.from(dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant());
 		
-		Date data = 
+		return Jwts.builder().setSubject(usuario.getLogin())
+										.setExpiration(data)
+										.signWith(SignatureAlgorithm.HS512,chaveAssinatura)
+										.compact();
 	}
-	
+
 }
